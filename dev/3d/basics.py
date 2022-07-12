fc13a8d069807a595fb8bd381a328e37d6de498f
gambino19 <michael.p.gambino@gmail.com>
2022-07-12 11:23:32 -0500

    Update basics.py

--- dev/3d/basics.py
+++ dev/3d/basics.py
@@ -62,36 +62,16 @@ class mesh:
 
         self.tris = np.array([arg.p for arg in args])
 
-    def __matmul__(self, other):
+    def w_matmul(self, other):
+        """ DOCSTRING """
         tris = np.append(self.tris, np.ones((len(self.tris), 3, 1)), axis=-1)
-        return MultiplyMatrixVector(tris, other)
+        o = tris @ other
+        tris, w = o[:,:,:3], o[:,:,-1].reshape((-1, 3, 1))
+        w = np.where(w==0, 1, w)
+        return tris / w
 
     def __repr__(self):
         return repr(self.tris.reshape((1,-1,9)))
-
-def rotation_matrices(matrix: str, theta: float = 0.0):
-    """ DOCSTRING """
-
-    if matrix == "x":
-        rotation_x = np.array([[1,  0            ,  0,             0],
-                               [0,  np.cos(theta), -np.sin(theta), 0],
-                               [0,  np.sin(theta),  np.cos(theta), 0],
-                               [0,  0,              0            , 1]])
-        return rotation_x
-    elif matrix == "y":
-        rotation_y = np.array([[np.cos(theta), 0, np.sin(theta), 0],
-                               [0,             1, 0            , 0],
-                               [np.sin(theta), 0, np.cos(theta), 0],
-                               [0,             0, 0            , 1]])
-        return rotation_y
-    elif matrix == "z":
-        rotation_z = np.array([[np.cos(theta), -np.sin(theta), 0, 0],
-                               [np.sin(theta),  np.cos(theta), 0, 0],
-                               [0,              0            , 1, 0],
-                               [0,              0            , 0, 1]])
-        return rotation_z
-    else:
-        raise ValueError("Provided matrix not available. Options: 'x', 'y', 'z'")
 
 def MultiplyMatrixVector(point, matrix):
     """ DOCSTRING """
@@ -105,8 +85,21 @@ def DrawTriangle(x1, y1, x2, y2, x3, y3, canvas):
     canvas.add(objects.Line((x2, y2), (x3, y3), div=2))
     canvas.add(objects.Line((x3, y3), (x1, y1), div=2))
 
-matRotZ = rotation_matrices('z', fTheta)
-matRotX = rotation_matrices('x', fTheta)
+matRotZ = np.zeros((4,4))
+matRotZ[0,0] = np.cos(fTheta)
+matRotZ[0,1] = np.sin(fTheta)
+matRotZ[1,0] = -np.sin(fTheta)
+matRotZ[1,1] = np.cos(fTheta)
+matRotZ[2,2] = 1.0
+matRotZ[3,3] = 1.0
+
+matRotX = np.zeros((4,4))
+matRotX[0,0] = 1.0
+matRotX[1,1] = np.cos(fTheta * 0.5)
+matRotX[1,2] = np.sin(fTheta * 0.5)
+matRotX[2,1] = -np.sin(fTheta * 0.5)
+matRotX[2,2] = np.cos(fTheta * 0.5)
+matRotX[3,3] = 1.0
 
 meshCube = mesh(
     # South
@@ -133,3 +126,13 @@ meshCube = mesh(
     triangle(vec3d(1.0, 0.0, 1.0), vec3d(0.0, 0.0, 1.0), vec3d(0.0, 0.0, 0.0)),
     triangle(vec3d(1.0, 0.0, 1.0), vec3d(0.0, 0.0, 0.0), vec3d(1.0, 0.0, 0.0)),
     )
+
+meshCube.tris = meshCube.w_matmul(matRotZ)
+meshCube.tris = meshCube.w_matmul(matRotX)
+meshCube.tris = meshCube.tris + np.repeat(np.array([[0.0, 0.0, 3.0]]), 3, axis=0) # Just need for Z
+meshCube.tris = meshCube.w_matmul(matProj)
+meshCube.tris = meshCube.tris + np.repeat(np.array([[1.0, 1.0, 0.0]]), 3, axis=0) # Just need for X and Y
+meshCube.tris = meshCube.tris * np.repeat(np.array([[0.5 * Width, 0.5 * Height, 1.0]]), 3, axis=0)
+for tri in meshCube.tris:
+    DrawTriangle(*tri[:, :-1].reshape(-1), canvas)
+canvas.show()
